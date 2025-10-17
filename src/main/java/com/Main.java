3b7bb3642eda44e876fffb66789398dc4e4992d6
jerrister <jerristerzheng@outlook.com>
2025-10-17 14:44:41 +0800

    fix hardcode and sql injection

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -5,6 +5,7 @@ import java.util.logging.Logger;
 
 public class Main {
 	// private static final String password = "123";
+	private static final String password;
 	private static final Logger logger = Logger.getLogger(Main.class.getName());
 
 	public int add(int a, int b) {
